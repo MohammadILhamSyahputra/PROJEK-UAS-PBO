3832d68356de21097673acb14d776acc33c45d54
Bagaseka27 <ekasaputrabagas62@gmail.com>
2025-12-02 03:25:28 +0700

    nambah user euy

--- user.py
+++ user.py
@@ -9,60 +9,62 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 
 class User:
-    def __init__(self):
-        pass
+    def __init__(self, username=None, nama=None, password=None, level=None):
+        self.username = username
+        self.nama = nama
+        self.password = password
+        self.level = level
 
-    def login(self, val1, val2): 
+    def login(self, username, password):
         sql = "SELECT level FROM user WHERE username = %s AND password = %s"
-        val = (val1, val2)
+        val = (username, password)
         mycursor.execute(sql, val)
-        level = mycursor.fetchone()
-        if level:
-            return str(level[0])
+        result = mycursor.fetchone()
+        if result:
+            return str(result[0])
         else:
-            return "Gagal" 
-        
-    def insert(self, val1, val2, val3, val4):
+            return "Gagal"
+
+    def insert(self):
         sql = "INSERT INTO user (username, nama_user, password, level) VALUES (%s, %s, %s, %s)"
-        val = (val1, val2, val3, val4)
+        val = (self.username, self.nama, self.password, self.level)
         mycursor.execute(sql, val)
         mydb.commit()
         print(mycursor.rowcount, "Data berhasil ditambahkan...")
 
-    def update(val1, val2, val3, val4): 
-        sql = "UPDATE user SET nama_user= %s, password= %s, level = %s WHERE username = %s"
-        val = (val1, val2, val3, val4)
+    def update(self, nama, password, level, username):
+        sql = "UPDATE user SET nama_user = %s, password = %s, level = %s WHERE username = %s"
+        val = (nama, password, level, username)
         mycursor.execute(sql, val)
         mydb.commit()
         print(mycursor.rowcount, "Data berhasil diupdate.")
 
-    def delete(val1):
+    def delete(self, username):
         sql = "DELETE FROM user WHERE username = %s"
-        value = (val1,)
-        mycursor.execute(sql, value)
+        val = (username,)
+        mycursor.execute(sql, val)
         mydb.commit()
-        print(mycursor.rowcount, "Data berhasil dihapus")
+        print(mycursor.rowcount, "Data berhasil dihapus.")
 
     def select_all(self):
-        sql = "SELECT username, password, nama_user, level FROM user"
+        sql = "SELECT username, nama_user, password, level FROM user"
         mycursor.execute(sql)
         return mycursor.fetchall()
-    
-    def select_by_id(val1):
+
+    def select_by_id(self, username):
         sql = "SELECT nama_user, username, password, level FROM user WHERE username = %s"
-        val = (val1)
+        val = (username,)
         mycursor.execute(sql, val)
-        myresult = mycursor.fetchone()
-        return myresult
+        return mycursor.fetchone()
 
     def search(self, keyword):
         sql = """
-            SELECT username, password, nama, level 
-            FROM user WHERE 
-                username LIKE %s OR
-                nama_user LIKE %s OR
-                level LIKE %s
+            SELECT username, nama_user, password, level 
+            FROM user 
+            WHERE username LIKE %s 
+               OR nama_user LIKE %s 
+               OR level LIKE %s
         """
-        val = ("%"+keyword+"%", "%"+keyword+"%", "%"+keyword+"%")
+        val = (f"%{keyword}%", f"%{keyword}%", f"%{keyword}%")
         mycursor.execute(sql, val)
         return mycursor.fetchall()
